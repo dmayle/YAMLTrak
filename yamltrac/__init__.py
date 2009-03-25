d755636e1373db63ec3fa1a689b9a69b96cb03da
Douglas Mayle http://douglas.mayle.org <devnull@localhost>
2009-03-25 08:51:10 -0400

    UPDATED: Ticket status FIXED: Initial support for ticket status

--- yamltrac/__init__.py
+++ yamltrac/__init__.py
@@ -11,7 +11,7 @@ def issues(repositories=[], dbfolder='issues', status=['open']):
     for repo in repositories:
         try:
             with open(path.join(repo, dbfolder, 'issues.yaml')) as issuesfile:
-                issues[path.basename(repo)] = dict(issue for issue in yaml.load(issuesfile.read()).iteritems() if issue[0] != 'skeleton')
+                issues[path.basename(repo)] = dict(issue for issue in yaml.load(issuesfile.read()).iteritems() if issue[0] != 'skeleton' and issue[1].get('status') == 'open')
         except IOError:
             # Not all listed repositories have an issue tracking database
             pass
