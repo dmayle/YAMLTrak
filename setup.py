58cd0a1e5ddcf601ea37c610efbf43f422b76776
Douglas Mayle http://douglas.mayle.org <devnull@localhost>
2009-04-16 15:42:19 -0400

    Switch back to setuptools now that we have a working monkey patch...

--- setup.py
+++ setup.py
@@ -16,70 +16,27 @@
 # along with YAMLTrak.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
-from distutils.core import setup as dutils_setup
+from setuptools import setup
+
 
 version = '0.5'
 
-def restore_distutils_install_script(self, dist, script_name, script_text, dev_path=None):
-    """\
-    Monkey patch setuptools with an install_script function that restores
-    distutils behavior."""
-    spec = str(dist.as_requirement())
-    is_script = is_python_script(script_text, script_name)
-
-    if is_script and dev_path:
-        script_text = get_script_header(script_text) + (
-            "# EASY-INSTALL-DEV-SCRIPT: %(spec)r,%(script_name)r\n"
-            "__requires__ = %(spec)r\n"
-            "from pkg_resources import require; require(%(spec)r)\n"
-            "del require\n"
-            "__file__ = %(dev_path)r\n"
-            "execfile(__file__)\n"
-        ) % locals()
-    elif is_script:
-        script_text = get_script_header(script_text) + (
-            "# EASY-INSTALL-SCRIPT: %(spec)r,%(script_name)r\n"
-            "__requires__ = %(spec)r\n"
-            "import pkg_resources\n"
-            "pkg_resources.run_script(%(spec)r, %(script_name)r)\n"
-        ) % locals()
-    self.write_script(script_name, script_text, 'b')
-
+# We monkeypatch setuptools to perform script installs the way distutils does.
+# Calling pkg_resources is too time intensive for a serious command line
+# applications.
 def install_script(self, dist, script_name, script_text, dev_path=None):
-    import pdb; pdb.set_trace()
     self.write_script(script_name, script_text, 'b')
 
-def hybrid_setup(**kwargs):
-    # In a hybrid approach, we don't want setuptools handling script install,
-    # unless on windows, as pkg_resource scans have too much overhead.
-    if sys.platform != 'win32':
-        # On most platforms, we'll use both approaches.
-        import pdb; pdb.set_trace()
-        if 'setuptools' in sys.modules:
-            # Someone used easy_install to run this.  I really want the correct
-            # script installed.
-            import setuptools.command.easy_install
-            setuptools.command.easy_install.install_script = install_script
-            setuptools.command.install_script = install_script
-
-        if 'develop' in sys.argv:
-            sys.argv[sys.argv.index('develop')] = 'install'
-            dutils_setup(**kwargs)
-            sys.argv[sys.argv.index('install')] = 'develop'
-
-            # Now that we've installed our script using distutils, we'll strip it
-            # out of the arguments and call setuptools for the rest.
-            if 'scripts' in kwargs:
-                del(kwargs['scripts'])
-
-            from setuptools import setup as stools_setup
-            stools_setup(**kwargs)
-        else:
-            dutils_setup(**kwargs)
+if sys.platform != 'win32' and 'setuptools' in sys.modules:
+    # Someone used easy_install to run this.  I really want the correct
+    # script installed.
+    import setuptools.command.easy_install
+    setuptools.command.easy_install.easy_install.install_script = install_script
 
 
 
-hybrid_setup(name='YAMLTrak',
+
+setup(name='YAMLTrak',
       version=version,
       description="YAMLTrak ('yt is on top of hg'), the issue tracker that uses mercurial as a database",
       long_description="""\
