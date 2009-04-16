c153198b35f07a20af2c7cd251b30cb7efde4ca4
Douglas Mayle http://douglas.mayle.org <devnull@localhost>
2009-04-16 15:49:39 -0400

    Fix the licensing notice... This is under the Lesser General Public License

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@
 # This file is part of YAMLTrak.
 
 # YAMLTrak is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
 
 # YAMLTrak is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU Lesser General Public License for more details.
 
-# You should have received a copy of the GNU General Public License
+# You should have received a copy of the GNU Lesser General Public License
 # along with YAMLTrak.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
@@ -32,8 +32,6 @@ if sys.platform != 'win32' and 'setuptools' in sys.modules:
     # script installed.
     import setuptools.command.easy_install
     setuptools.command.easy_install.easy_install.install_script = install_script
-
-
 
 
 setup(name='YAMLTrak',
